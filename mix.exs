29afe607e6b3a574be68d7f86a5615a741574798
Anton Maminov <anton.maminov@gmail.com>
2016-11-16 20:41:56 +0200

    :timex, "~> 3.0"

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule Rackspace.Mixfile do
     [
       {:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.2", override: true},
       {:httpotion, "~> 3.0"},
-      {:timex, "~> 2.1"},
+      {:timex, "~> 3.0"},
       {:poison, "~> 2.0"}
     ]
   end
